01cd3d82efe2c7fdfcc28f908aa66157a12562d0
eclevens <ellaclevens@gmail.com>
2024-09-24 09:05:21 -0400

    initial files entered

---
+++ week5a/week5a/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace week5a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int myFavNum = 3;
+            int theBestNum = 4;
+
+            int sum;
+            sum = myFavNum + theBestNum;
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
